9832c90d2d434495109971564cb17aeaf346dccf
andre059 <andreyperm159@mail.ru>
2023-11-05 14:50:50 +0500

    5.3 (создание рассылки)

---
+++ car/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+
+from car.models import Car, Owner, CarHistory
+
+
+class FormStyleMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class CarForm(FormStyleMixin, forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+
+        exception_words = ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно',
+                           'обман', 'полиция', 'радар')
+        for word in cleaned_data.split():
+            if word.lower() in exception_words:
+                raise forms.ValidationError('Такое слово нельзя вводить !!!')
+
+        return cleaned_data
+
+
+class OwnerForm(FormStyleMixin, forms.ModelForm):
+    class Meta:
+        model = Owner
+        fields = '__all__'
+
+    def clean_email(self):
+        cleaned_data = self.cleaned_data['email']
+
+        if cleaned_data:
+            if not cleaned_data.endswith('@') and '@' not in cleaned_data:
+                raise forms.ValidationError('Должен быть введен адрес почты ')
+        else:
+            raise forms.ValidationError('Должен быть введен адрес почты ')
+        return cleaned_data
+
+
+class CarHistoryForm(FormStyleMixin, forms.ModelForm):
+    class Meta:
+        model = CarHistory
+        fields = '__all__'
--- car/views.py
+++ car/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
+from car.forms import CarForm
 from car.models import Car
 
 
@@ -10,3 +13,17 @@ class CarListView(ListView):
 
 class CarDetailView(DetailView):
     model = Car
+
+
+class CarCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Car
+    form_class = CarForm
+    permission_required = ['car.add_car']
+    success_url = reverse_lazy('car:home')
+
+
+class CarUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = Car
+    form_class = CarForm
+    permission_required = ['car.change_car']
+    template_name = 'car/car_form_with_formset.html'
