a00e443c44c2dd47004f6eb8c54d13b647058645
andre059 <andreyperm159@mail.ru>
2023-11-12 22:42:38 +0500

    9.1 (создание блога)

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import PostView
+from blog.views import PostView, PostDetail
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('', PostView.as_view(template_name='blog/blog.html'), name='post'),
+    path('', PostView.as_view(), name='post'),
+    path('<int:pk>/', PostDetail.as_view(), name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,11 +8,17 @@ class PostView(View):
     """Вывод записей"""
 
     model = Post
-    template_name = 'blog/blog.html'
+    # template_name = 'blog/blog.html'
 
     def get(self, request):
         posts = Post.objects.all
-        return render(request, 'blog/blog.html', {'posts': posts})
+        return render(request, 'blog/blog.html', {'post_list': posts})
 
 
+class PostDetail(View):
+    """Отдельная страница записи"""
+    model = Post
 
+    def get(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        return render(request, 'blog/blog_detail.html', {'post': post})
